e34a00436477731544d3674d9746de832513747a
sayanmndl21 <sayanmndl21@gmail.com>
2018-07-12 11:49:22 -0400

    window added

--- detectdrone.py
+++ detectdrone.py
@@ -5,6 +5,7 @@ import librosa
 import sounddevice as sd
 import time as tm
 import scipy.io.wavfile as wavf
+import curses
 
 from feature_extraction import filters as fil
 from feature_extraction import lpcgen as lpg
@@ -17,8 +18,19 @@ from sklearn import svm
 from sklearn.externals import joblib
 import pickle
 
+curses.initscr()
+
 clf = joblib.load('input/detection_iris_new.pkl')
 clf1 = joblib.load('input/detect_irisdrone_new.pkl')
+
+rows = 10
+cols = 60
+winlist = []
+
+win = curses.newwin(rows,cols, 10, 3)
+win.clear()
+win.border()
+winlist.append(win)
 
 def record(time = 1, fs = 44100):
     #os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'feature_extraction'))
@@ -77,17 +89,26 @@ while True:
         a,e,k = lpg.lpc(ns,10)
         mfcc_test = par.get_parsed_mfccdata(mfcc, chroma,mel,spect,tonnetz)
         lpc_test = par.get_parsed_lpcdata(a,k,p)
-        sys.stdout.write("\r Maybe a drone... Please Wait \r \r \r \n \r")
+        win.addstr(3,5,"Maybe a drone... Please Wait")
         x1 = clf.predict(mfcc_test)
         x2 = clf1.predict(lpc_test)
-        sys.stdout.write('\r The drone is %s \r \r \n \r'% dist_prediction_label(x1[0]))
-        sys.stdout.write('\r To be sure there is a %s \r \r \n \r'% drone_prediction_label(x2[0]))
-        sys.stdout.flush()
+        win.addstr(5,5,"The drone is %s "% dist_prediction_label(x1[0]))
+        win.addstr(6,5,"To be sure there is a %s "% drone_prediction_label(x2[0]))
+        #sys.stdout.write("\r Maybe a drone... Please Wait \r \r \r \n \r")
+        #sys.stdout.write('\r The drone is %s \r \r \n \r'% dist_prediction_label(x1[0]))
+        #sys.stdout.write('\r To be sure there is a %s \r \r \n \r'% drone_prediction_label(x2[0]))
+        #sys.stdout.flush()
     else:
-        sys.stdout.write("\ n \r Need time to compute, but I think there is no drone \r \r \r \n")
-        sys.stdout.flush()
+        win.addstr(3,5,"Maybe a drone... Please Wait")
+        win.addstr(5,5,"Need time to compute, but I think there is no drone")
+        #sys.stdout.write("\ n \r Need time to compute, but I think there is no drone \r \r \r \n")
+        #sys.stdout.flush()
     i+=1
+    
     if sys.stdin in select.select([sys.stdin],[],[],0)[0]:
         line = input()
+        curses.endwin()
         break
+    win.refresh()
+    
 print('iter_num:',i)
